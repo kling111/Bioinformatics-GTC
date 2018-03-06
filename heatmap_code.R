1a48f7ab0b9f882f8a139625987407b816679474
Karan Lingineni <kling111@gmail.com>
2018-03-05 19:25:33 -0600

    Update heatmap_code.R
    Made more reproducible, with better comments and variable names

--- heatmap_code.R
+++ heatmap_code.R
@@ -1,50 +1,41 @@
+library(readr)
+mouse1_data <- read_csv("~/Documents/My Documents/Bioinformatics proj/mouse1_data.csv") #You may have to change this to find the csv.file in your system
 View(mouse1_data) #or any dataset name you want to make a heatmap out of.
 
-aaVector <- AAStringSet()
-holder <- AAStringSet()
+freqMatrix <- c()
+freqArray <- c()
+count <- 0
 
 # loops from first value to last value, creating a AAstringset of all sequences
 # 2:8759 is specific for this dataset, in general it should be firstValue:lastValue.
-for (num in 2:8759)
+
+for(len in 1:25) #25 is the longest length of a sequence in the mouse data
 {
-  holder = AAStringSet(mouse1_data$X2[num])
-  
-  aaVector = append(aaVector, holder)
-  
-}
-
-consensusOriginal <- consensusMatrix(aaVector)
-consensusFinal <- consensusMatrix(aaVector)
-
-# switches the order of the rows from A to Y , Y to A
-for(num in 1:nrow(consensusOriginal))
-{
-  consensusFinal[num, ] = consensusOriginal[nrow(consensusOriginal) - (num - 1), ]
-}
-
-logConsensus <- log10(consensusFinal)
-
-# removes all the -Inf values from the log consensus
-for(num in 1:nrow(consensusOriginal))
-{
-  for(sec in 1:ncol(consensusOriginal))
+  for (amino in c("Y","X","W","V","T","S","R","Q","P","N","M","L","K","I","H","G","F","E","D","C","A"))
   {
-    if(logConsensus[num, sec] == -Inf)
-    {
-      logConsensus[num, sec] = 0
+    for(num in 2:8759)
+    {  
+      if(substr(mouse1_data$X2[num], len, len) == amino)
+      {
+        count = as.double(mouse1_data$X3[num]) + count
+      }
     }
+    freqArray <- append(freqArray, count)
+    count <- 0
   }
+  freqMatrix <- cbind(freqMatrix, freqArray)
+  freqArray <- c()
 }
 
 #renames the row names to correspond to the correct amino acid.
-row.names(logConsensus) <- c("Y","X","W","V","T","S","R","Q","P","N","M","L","K","I","H","G","F","E","D","C","A")
-
+row.names(freqMatrix) <- c("Y","X","W","V","T","S","R","Q","P","N","M","L","K","I","H","G","F","E","D","C","A")
+colnames(freqMatrix) <- c(1:25)
 #creates a heatmap that scales by the row.
-heatmap(logConsensus, Colv = NA, Rowv = NA, scale = "row", margins = c(5,10),
+heatmap(freqMatrix, Colv = NA, Rowv = NA, scale = "row", margins = c(5,10),
         xlab = "Position in Sequence", ylab =  "Amino Acid",
         main = "GeneLab")
 
 #creates a heatmap that scales by the column.
-heatmap(logConsensus, Colv = NA, Rowv = NA, scale = "col", margins = c(5,10),
+heatmap(freqMatrix, Colv = NA, Rowv = NA, scale = "col", margins = c(5,10),
         xlab = "Position in Sequence", ylab =  "Amino Acid",
         main = "GeneLab")
