57a28d0b032b72e80f783ed68f1d15bd8321c127
Karan Lingineni <kling111@gmail.com>
2018-03-07 00:59:29 -0600

    Add files via upload

---
+++ mouseData_H2O.R
@@ -0,0 +1,21 @@
+#Here I find the urlPath of the mouse on my system
+#Then I upload that csv file to the h2o directory
+mousePath = "/Users/karanlingineni/Documents/My\ Documents/Bioinformatics\ proj/mouse1_data.csv"
+mouse.hex = h2o.importFile(path = mousePath)
+
+#The anyFactor() function helps us figure out if there are any columns in the file that contain categorical data
+h2o.anyFactor(mouse.hex)
+
+#This allows us to view the data, now in a 5-number summary.
+summary(mouse.hex)
+
+#This function creates a more specific quantile number summary
+quantile(x = mouse.hex$C3, na.rm = TRUE)
+
+#This h2o function creates a histogram from a file's given factor, in this case sepal_len
+h2o.hist(mouse.hex$C3)
+#However, in this case it's better to use the log10() of the mouse data's frequencies
+h2o.hist(log10(mouse.hex$C3))
+
+
+
