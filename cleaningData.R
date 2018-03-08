c893febb8332c74a7683fb383eee842d1f25d951
Karan Lingineni <kling111@gmail.com>
2018-03-07 20:31:06 -0600

    Add files via upload

--- cleaningData.R
+++ cleaningData.R
@@ -1,8 +1,8 @@
 library(rlist)
+
 library(readxl)
-Pool_1_CDR3_Results <- read_excel("~/Downloads/Chapes files/Pool 1 - CDR3 Results.xlsx", 
-                                  col_names = FALSE)
-View(Pool_1_CDR3_Results) #These lines of code were used to read in a excel file that contains AA Junctions with many duplicates
+Pool_1_CDR3_Results <- read_excel("~/Downloads/Chapes files/Pool 1 - CDR3 Results.xlsx", col_names = FALSE)
+View(Pool_1_CDR3_Results) #Hello, Ms, Jacci, all you need to do is change these three lines of code right here.
 
 #Here, I take the csv file and format the sequence into a list format
 #Pool_1_CDR3_Results is the name of the csv file, and X__1 is the column that contains the amino sequences
@@ -24,14 +24,14 @@ list1 <- unique(list1)
 
 #creating a blank AAStringSet, and filling it with the now unique sequences
 pool1AA <- AAStringSet()
-for(i in temp)
+for(i in list1)
   {for(j in i)
     {u <- AAStringSet(j)
     pool1AA <- append(pool1AA, u)
 }
 }
 
-print(pool1AA)
+pool1AA
 #from here you can make heatmaps of your data by going through the heatmap code 
 #However, remember some datasets need their duplicates to make an accurate heatmap
 #For example, the Chapes data does not have a frequency column, therefore the duplicated for that dataset are necessary.
